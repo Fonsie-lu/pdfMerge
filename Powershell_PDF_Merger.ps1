d9f80a1c6c3e66d82e219b2bbc794a89d5596837
Beat Weber <beat.weber1@swisscom.com>
2020-04-19 19:24:15 +0200

    Bugfix rerun

--- Powershell_PDF_Merger.ps1
+++ Powershell_PDF_Merger.ps1
@@ -1,30 +1,41 @@
-﻿Install-Module PSWritePDF  -Force
+﻿#Changeable Parameters
+$separator = "_"
+$separatorIndex = 3
+$removeText = "_KarteNr_"
+
+#Declare
+Install-Module PSWritePDF
+Import-Module PSWritePDF
 $ExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy Bypass
+$pdf = gci | Where-Object {$_.Extension -eq ".pdf" -and $_.Name -match $removeText}| Sort-Object -Property Name
 $i =0
 clear
 
-$pdf = gci | Where-Object {$_.Extension -eq ".pdf"}| Sort-Object -Property Name
+#Generate merged PDF in Subfolders
 foreach ($file in $pdf) {
-$i++;
-$j = $i-1;
-Import-Module PSWritePDF
-$createDir = ( $file.Name.Split('_')[3])
-mkdir $createDir -Force > $null
-$targetFile = $file.DirectoryName + "\" + $createDir + "\" + $file.Name
-Copy-Item $file.FullName $targetFile 
-$mergedPDF = $targetFile  -replace '\d','' -replace '_KarteNr_'
-Merge-PDF -InputFile $mergedpdf$j, $targetFile -OutputFile $mergedPDF$i > $null
+  $i++;
+  $j = $i-1;
+  $createDir = ( $file.Name.Split($separator)[$separatorIndex])
+  mkdir $createDir -Force > $null
+  $targetFile = $file.DirectoryName + "\" + $createDir + "\" + $file.Name
+  Copy-Item $file.FullName $targetFile 
+  $mergedPDF = $targetFile  -replace '\d','' -replace $removeText 
+  Merge-PDF -InputFile $mergedpdf$j, $targetFile -OutputFile $mergedPDF$i > $null
 }
-
+  
+#Copy back latest PDF and cleanup Subfolders / PDF's
 $pdfPath = gci -Directory
 foreach($mergePath in $pdfPath){
-cd $mergePath.FullName
-$chosenPDF = gci $mergePath.FullName | sort LastWriteTime | select -last 1
-Copy-Item $chosenPDF ../ 
-cd ..
-Remove-Item $mergePath -Force -Recurse
-Move-Item $chosenPDF ($chosenPDF -replace '\d') -Force
+  cd $mergePath.FullName
+  $chosenPDFtemp = (gci $mergePath.FullName | sort LastWriteTime | select -last 1) 
+  $chosenPDF = $chosenPDFtemp -replace '\d'
+  Move-Item $chosenPDFtemp ../$chosenPDF -Force
+  cd ..
+  Remove-Item $mergePath -Force -Recurse
+  write-host `PDF generated: $chosenPDF`
 }
+
+#Cleanup & Finish
 Set-ExecutionPolicy -ExecutionPolicy $ExecutionPolicy
 write-host "done"
