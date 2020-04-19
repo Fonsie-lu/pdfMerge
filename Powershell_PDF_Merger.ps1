af771f01a741cdf429c4df810251218426dc42ae
Beat Weber <beat.weber.86@gmail.com>
2020-04-19 15:35:31 +0200

    Prettier & Readme

--- Powershell_PDF_Merger.ps1
+++ Powershell_PDF_Merger.ps1
@@ -1,30 +1,42 @@
-﻿Install-Module PSWritePDF  -Force
+﻿#changeable Parameters
+$separator = "_"
+$separatorIndex = "3"
+$removeText = "_KarteNr_"
+
+#Declare
+Install-Module PSWritePDF  -Force
+Import-Module PSWritePDF
 $ExecutionPolicy = Get-ExecutionPolicy
 Set-ExecutionPolicy -ExecutionPolicy Bypass
+$pdf = gci | Where-Object {$_.Extension -eq ".pdf"}| Sort-Object -Property Name
+$pdfPath = gci -Directory
 $i =0
 clear
 
-$pdf = gci | Where-Object {$_.Extension -eq ".pdf"}| Sort-Object -Property Name
+#Generate merged PDF in Subfolders
 foreach ($file in $pdf) {
-$i++;
-$j = $i-1;
-Import-Module PSWritePDF
-$createDir = ( $file.Name.Split('_')[3])
-mkdir $createDir -Force > $null
-$targetFile = $file.DirectoryName + "\" + $createDir + "\" + $file.Name
-Copy-Item $file.FullName $targetFile 
-$mergedPDF = $targetFile  -replace '\d','' -replace '_KarteNr_'
-Merge-PDF -InputFile $mergedpdf$j, $targetFile -OutputFile $mergedPDF$i > $null
+  $i++;
+  $j = $i-1;
+  $createDir = ( $file.Name.Split($separator)[$separatorIndex])
+  mkdir $createDir -Force > $null
+  $targetFile = $file.DirectoryName + "\" + $createDir + "\" + $file.Name
+  Copy-Item $file.FullName $targetFile 
+  $mergedPDF = $targetFile  -replace '\d','' -replace $removeText 
+  Merge-PDF -InputFile $mergedpdf$j, $targetFile -OutputFile $mergedPDF$i > $null
+}
+  
+#Copy back latest PDF and cleanup Subfolders / PDF's
+foreach($mergePath in $pdfPath){
+  cd $mergePath.FullName
+  $chosenPDF = (gci $mergePath.FullName | sort LastWriteTime | select -last 1) -replace '\d'
+  Copy-Item $chosenPDF ../ 
+  cd ..
+  Remove-Item $mergePath -Force -Recurse
+  Move-Item $chosenPDF -Force
+  write-host "PDF generated: "$chosenPDF
 }
 
-$pdfPath = gci -Directory
-foreach($mergePath in $pdfPath){
-cd $mergePath.FullName
-$chosenPDF = gci $mergePath.FullName | sort LastWriteTime | select -last 1
-Copy-Item $chosenPDF ../ 
-cd ..
-Remove-Item $mergePath -Force -Recurse
-Move-Item $chosenPDF ($chosenPDF -replace '\d') -Force
-}
+#Cleanup & Finish
 Set-ExecutionPolicy -ExecutionPolicy $ExecutionPolicy
 write-host "done"
+pause
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Run
+
+[ ] place script in the folder, where all the pdf's are (do not use subfolders)
+[ ] Run Script as Administrator (Need to install / import Moudles)
+
+# Configuration / Restrictions
+
+- the first 3 Lines in the script can be changed (separator, separatorIndex, removeText)
+- All Numbers in the Filename will be lost
+- Merging is based on grouped Separator and sorted by Filename
